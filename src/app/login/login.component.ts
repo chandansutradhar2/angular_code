b8e1806d851b55b145ae40cba34a4a21da8b89f4
chandan naresh <chandansutradhar2@gmail.com>
2021-07-29 15:57:09 +0530

    error message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 email:string="";
 password:string="";
+emailerr:string="";
+passworderr:string="";
   
   constructor() { }
 
@@ -19,10 +21,21 @@ password:string="";
     console.log('password toggle clicked');
   }
   login(){
-    console.log(this.email,this.password);
-    alert('login failed. pls try again');
-    this.email="";
-  
+    
+  }
+
+  validate(){
+    
+    if(this.email.length<=0){this.emailerr="email cannot be left blank"; return}else{
+      this.emailerr="";
+    }
+    let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(regex.test(this.email)){
+      this.emailerr="";
+    }else{
+      this.emailerr="email format is incorrect"
+    }
   }
 
 }
